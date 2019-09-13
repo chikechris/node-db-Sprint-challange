6bf6cfc8d50bfe653acff611a66612568f69b299
chikechris <cchike2017@gmail.com>
2019-09-13 13:20:24 -0400

    added post routes for projects

---
+++ routes/tasksModel.js
@@ -0,0 +1,26 @@
+const db = require('../data/db-config.js')
+
+function getTasks () {
+  return db('tasks')
+    .join('projects', 'tasks.project_id', '=', 'projects.id')
+    .select(
+      'tasks.id',
+      'tasks.description',
+      'tasks.completed',
+      'tasks.notes',
+      'projects.project_name'
+    )
+}
+
+function getTasksByProjectId (project_id) {
+  return db('tasks')
+    .where({ project_id })
+    .select('tasks.id', 'tasks.description', 'tasks.completed', 'tasks.notes')
+}
+
+function postTask (taskPost) {
+  return db('tasks').insert(taskPost)
+}
+
+module.exports = { getTasks, getTasksByProjectId, postTask }
+
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const taskRoute = require('./routes/tasksRoute.js')
+const projectRoute = require('./routes/projectsRoute.js')
 
 const server = express()
 
@@ -7,9 +8,12 @@ server.use(express.json())
 
 server.use('/api/tasks', taskRoute)
 
+server.use('/api/projects', projectRoute)
+
 server.use('/', (req, res) => {
   res.send(`<h2>API Home Sprint</h2>`)
 })
 
 module.exports = server
+
 
