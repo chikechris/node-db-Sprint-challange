1d4991c0386fdface23f45aaa30d70f6a111aa29
chikechris <cchike2017@gmail.com>
2019-09-13 12:17:09 -0400

    added schema to migrations file

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const taskRoute = require('./routes/tasksRoute.js')
 
 const server = express()
 
 server.use(express.json())
 
+server.use('/api/tasks', taskRoute)
+
 server.use('/', (req, res) => {
-  res.send(`<h2>Api Home Sprint</h2>`)
+  res.send(`<h2>API Home Sprint</h2>`)
 })
 
 module.exports = server
+
